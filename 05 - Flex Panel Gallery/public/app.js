10f6ff9a1d35bdc3d79e82ed63bc8f7e7a634fd1
James Amos <jamos_98@hotmail.com>
2019-03-08 13:42:34 +0000

    app.js file created

---
+++ 05 - Flex Panel Gallery/public/app.js
@@ -0,0 +1,11 @@
+window.onload(function(){
+	const panels = document.querySelectorAll('.panels');
+
+	function toggleOpen() {
+		this.classList.toggle('open');
+	}
+
+	panels.forEach(panel=> panel.addEventListener('click', toggleOpen));
+
+
+})
